15a31eb3215fabc117263096b4f895c47181a823
Bronco Oostermeyer <b.oostermeyer@flusso.nl>
2024-05-26 12:12:35 +0200

    added minor-version, updated docs & test

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # openegde-flatversion
 Github action turning an OpenEdge version into a flat version string (12.8.2 --> 128)
+
+## usage 
+
+```
+- name: Check unit tests
+  id: unit-test
+  uses: bfv/openedge-flatversion@v1.1.0
+  with:
+    version: '12.8.2'
+```
+The results can be used like:
+```
+echo "flat-version is ${{ steps.unit-test.outputs.flat-version }}"
+echo "release-version is ${{ steps.unit-test.outputs.release-version }}"
+```
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const core = require('@actions/core');
 
-
 try {
   const version = core.getInput('version');
   const parts = version.split('.');
@@ -8,8 +7,12 @@ try {
     throw new Error('Invalid version format');
   }
   const flatVersion = parts[0] + parts[1];
-  console.log(`${version} --> ${flatVersion}`);
+  console.log(`flat-version: ${version} --> ${flatVersion}`);
   core.setOutput('flat-version', flatVersion);
+
+  const releaseVersion = parts[0] + "." + parts[1];
+  console.log(`release-version: ${version} --> ${releaseVersion}`);
+  core.setOutput('release-version', releaseVersion);
 }
 catch (error) {
   core.setFailed(error.message);
